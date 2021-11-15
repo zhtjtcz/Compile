8d0791d46f222f71ca7c3e8c786942da81fcf03a
zhtjtcz <18377221@buaa.edu.cn>
2021-11-15 20:16:19 +0800

    fix: main.py

--- src/main.py
+++ src/main.py
@@ -8,13 +8,14 @@ def main():
 	input = inputfile.read() + '\n'
 	input = tool.remove(input)
 	print(input)
-	lexer = lexical.laxer.getLexer()
-	result = syntactic.analyze.getAnalyzer(input, lexer)
-	# result.dfs_test(result, 0, None)
 	print('declare i32 @getint()', file = outputFile)
 	print('declare void @putint(i32)', file = outputFile)
 	print('declare i32 @getch()', file = outputFile)
 	print('declare void @putch(i32)\n', file = outputFile)
+	
+	lexer = lexical.laxer.getLexer()
+	result = syntactic.analyze.getAnalyzer(input, lexer)
+	# result.dfs_test(result, 0, None)
 	dfs(result)
 
 if __name__ == "__main__":
