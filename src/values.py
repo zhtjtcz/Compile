587420682862bcb666e287c4cd1972f9b0b52d80
zhtjtcz <18377221@buaa.edu.cn>
2021-11-19 22:48:22 +0800

    fix: values

--- src/values.py
+++ src/values.py
@@ -2,13 +2,11 @@ import os
 import sys
 import platform
 
+LOCAL = False
 if platform.system()=='Windows':
 	LOCAL = True
 elif 'USER' in os.environ.keys():
 	LOCAL = (os.environ['USER'] == 'oem')
-else:
-	LOCAL = False
-# ENCODING = 'utf-8'
 
 if LOCAL:
 	inputfile = open('a.c', 'r')
