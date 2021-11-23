e115bf691f3718a835f9d8b6497820d24c98c849
zhtjtcz <18377221@buaa.edu.cn>
2021-11-23 08:47:13 +0800

    add: new fucntion declear

--- src/main.py
+++ src/main.py
@@ -9,7 +9,9 @@ def main():
 	print('declare i32 @getint()', file = outputFile)
 	print('declare void @putint(i32)', file = outputFile)
 	print('declare i32 @getch()', file = outputFile)
-	print('declare void @putch(i32)\n', file = outputFile)
+	print('declare void @putch(i32)', file = outputFile)
+	print('declare i32 @getarray(i32*)', file = outputFile)
+	print('declare void @putarray(i32, i32*)\n', file = outputFile)
 	
 	lexer = lexical.laxer.getLexer()
 	result = syntactic.analyze.getAnalyzer(input, lexer)
